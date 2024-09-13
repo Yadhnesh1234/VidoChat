d3ef6891368678468f5009850f63c8376d7b83e6
Yadhnesh1234 <gangurdeyadhnesh28@gmail.com>
2024-09-13 18:55:39 +0530

    Socket Added

--- server/index.js
+++ server/index.js
@@ -1,3 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {server} =  require('socket.io');
+const {Server} =  require('socket.io');
+
+const io = new Server({
+    cors:true
+});
+const app=express();
+
+app.use(bodyParser.json());
+
+const emailToSocketMap = new Map();
+
+io.on("connection",(socket)=>{
+    console.log("New Connection")
+    socket.on("join-room",(data)=>{
+        console.log("user",data);
+        const {roomId,username}=data;
+        emailToSocketMap.set(username,socket.id)
+        socket.join(roomId);
+        socket.broadcast.to(roomId).emit("user-joined",{username})
+    })
+})
+
+io.on('connection',(socket)=>{})
+
+app.listen(8000,()=> console.log("Server Running on port 8000"));
+io.listen(8001);
